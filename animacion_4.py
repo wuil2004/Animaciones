28822d008e7304d2a0abae88175e01ee728a18a6
williams <williamsmatiassan@gmail.com>
2024-09-28 22:02:05 -0600

    cambio2

--- animacion_4.py
+++ animacion_4.py
@@ -2,8 +2,9 @@ import turtle
 
 # Crear objeto turtle
 t = turtle.Turtle()
-t.speed(1)  # Ajustar la velocidad de dibujo
-t.pensize(5)
+t.shape("turtle")  # Cambiar la forma a una tortuga
+t.speed(1)  # Ajustar la velocidad para ver mejor el dibujo
+t.pensize(5)  # Tamaño de la pluma
 
 # Función para dibujar la letra 'P'
 def draw_p():
@@ -12,13 +13,13 @@ def draw_p():
     t.right(90)
     t.circle(-30, 180)
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor() - 100)
+    t.goto(t.xcor() + 40, t.ycor() - 100)
     t.pendown()
 
 # Función para dibujar la letra 'Y'
 def draw_y():
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor())
+    t.goto(t.xcor() + 40, t.ycor())
     t.pendown()
     t.forward(50)
     t.right(45)
@@ -27,13 +28,13 @@ def draw_y():
     t.right(90)
     t.forward(50)
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor() - 100)
+    t.goto(t.xcor() + 40, t.ycor() - 100)
     t.pendown()
 
 # Función para dibujar la letra 'T'
 def draw_t():
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor() + 100)
+    t.goto(t.xcor() + 40, t.ycor() + 100)
     t.pendown()
     t.forward(50)
     t.backward(25)
@@ -54,13 +55,13 @@ def draw_h():
     t.forward(50)
     t.backward(100)
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor() - 100)
+    t.goto(t.xcor() + 40, t.ycor() - 100)
     t.pendown()
 
 # Función para dibujar la letra 'O'
 def draw_o():
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor())
+    t.goto(t.xcor() + 40, t.ycor())
     t.pendown()
     t.circle(30)
     t.penup()
@@ -76,7 +77,7 @@ def draw_n():
     t.left(135)
     t.forward(100)
     t.penup()
-    t.goto(t.xcor() + 20, t.ycor() - 100)
+    t.goto(t.xcor() + 40, t.ycor() - 100)
     t.pendown()
 
 # Función para escribir PYTHON
@@ -88,29 +89,23 @@ def write_python():
     draw_o()
     draw_n()
 
-# Función para escribir TURTLE
-def write_turtle():
-    t.setheading(0)  # Restablecer dirección de la tortuga
+# Función para dibujar la letra 'U'
+def draw_u():
     t.penup()
-    t.goto(-200, -50)  # Ajustar la posición de inicio
+    t.goto(t.xcor() + 20, t.ycor() + 100)
     t.pendown()
-
-    # Dibujar las letras de "TURTLE" de manera similar
-    draw_t()  # Letra T
-    draw_u()  # Letra U
-    draw_r()  # Letra R
-    draw_t()  # Letra T
-    draw_l()  # Letra L
-    draw_e()  # Letra E
+    t.forward(70)
+    t.backward(100)
+    t.circle(-40, -180)
+    t.penup()
+    t.goto(t.xcor() + 40, t.ycor() - 100)
+    t.pendown()
 
 # Dibujo de la palabra "PYTHON"
 t.penup()
-t.goto(-200, 100)  # Ajustar la posición de inicio
+t.goto(-300, 100)  # Ajustar la posición de inicio
 t.pendown()
 write_python()
 
-# Dibujo de la palabra "TURTLE"
-write_turtle()
-
-# Finalizar animación
+# Terminar animación
 turtle.done()
