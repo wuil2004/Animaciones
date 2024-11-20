15c3f95b4483284e83d265a896ecb91e2e3dd9cf
williams <williamsmatiassan@gmail.com>
2024-11-19 21:18:44 -0600

    kiki

--- pirotecnia/music_piro.py
+++ pirotecnia/music_piro.py
@@ -108,7 +108,7 @@ class Particle:
 
 # Configuración de sincronización de fuegos artificiales
 clock = pygame.time.Clock()
-beat_interval = 500  # Intervalo en milisegundos entre explosione
+beat_interval = 500  # Intervalo en milisegundos entre explosiones
 
 fireworks = []
 running = True
